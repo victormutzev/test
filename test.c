e7aebd592644fd54ee255bb2fd102058a2dd2d1b
victormutzev <mutzev@yahoo.com>
2016-02-09 22:09:42 +0200

    revert na baba mu

--- test.c
+++ test.c
@@ -8,17 +8,4 @@ main()
  printf("row1\n");
  printf("row1\n");
  printf("row1\n");
-<<<<<<< HEAD
- 
- printf("last");
- 
- 
- 
- 
- printf("add last1");
- printf("add last2");
- printf("add last3");
-
-=======
->>>>>>> parent of c044399... add last row
 }
