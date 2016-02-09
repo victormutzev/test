0331d0008fbf21e37c1893488921c12d7cb99769
victormutzev <mutzev@yahoo.com>
2016-02-09 20:56:08 +0200

    revert row 2

--- test.c
+++ test.c
@@ -3,9 +3,12 @@
 main()
 {
  printf("row1\n");
+<<<<<<< HEAD
  printf("row2\n");
  printf("row3\n");
  printf("row4\n");
  printf("row5\n");
  printf("row6\n");
+=======
+>>>>>>> parent of 6c51e42... row 2
 }
